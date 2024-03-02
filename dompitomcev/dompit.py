517b511ffc28be7ae61cee781d5f4b47dbf389f9
ittspex <kryspex@web.de>
2024-03-02 14:56:58 +0100

    Initial commit

---
+++ dompitomcev/dompit.py
@@ -0,0 +1,5 @@
+class CAT:
+    def __init__(self, name, pol, age):
+        self.name = name
+        self.pol = pol
+        self.age = age
---
+++ dompitomcev/dompit2.py
@@ -0,0 +1,5 @@
+from dompit import CAT
+cat1 = CAT("Baron", "Kot", 2)
+cat2 = CAT("Sam", "Kot", 2)
+print(cat1.name, cat1.pol, cat1.age)
+print(cat2.name, cat2.pol, cat2.age)
